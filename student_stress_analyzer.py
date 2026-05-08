5928151dd246fb0510a6fb2e2cf48f5740412f2c
Bhumikha R <bhumikhagowda28@gmail.com>
2026-05-08 19:58:58 +0530

    Create student_stress_analyzer.py

---
+++ student_stress_analyzer.py
@@ -0,0 +1,6 @@
+sleep=int(input("enter sleep hours: "))
+screen=int(input("enter screen hours: "))
+study=int(input("enter study hours: "))
+
+if sleep<5 and screen>8:
+	print("High stress level detected")
