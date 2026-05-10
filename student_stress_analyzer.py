fb415268a1b23d9b8b0d42888c32bb21841b3834
Bhumikha R <bhumikhagowda28@gmail.com>
2026-05-10 18:55:59 +0530

    Update student_stress_analyzer.py

--- student_stress_analyzer.py
+++ student_stress_analyzer.py
@@ -7,3 +7,7 @@ if sleep<5 and screen>8:
  print(" take proper rest and reduce screen   time")
 elif sleep<=7:
 	print(" medium stress detected")
+ print(" try balancing the same routines")
+else:
+	print("low stress detected")
+	print(" keep maintaining healthy habits")
